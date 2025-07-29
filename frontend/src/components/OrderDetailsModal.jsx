8c6b51bb7813d4929a0b348384ea5adc3af1a932
Muhammad Ali Mustafa <ali.mustafa@qbatch.com>
2025-07-29 12:26:32 +0500

    Add total bill label in OrderDetailsModal Component

--- frontend/src/components/OrderDetailsModal.jsx
+++ frontend/src/components/OrderDetailsModal.jsx
@@ -43,14 +43,14 @@ function OrderDetailsModal({ isOpen, onClose, order }) {
                             <textarea disabled id="instructions" name="instructions" rows="4" value={order.instructions} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                         </div>
                         <div className="col-span-1">
-                            <label htmlFor="bill" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white text-left" >
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white text-left" >
                                 Total Bill: 
                             </label>
                         </div>
                         <div className="col-span-1">
-                                <div className="text-right">
-                                    {order?.bill } PKR
-                                </div>
+                            <div className="text-right">
+                                {order?.bill } PKR
+                            </div>
                         </div>
                     </div>
                 </div>
