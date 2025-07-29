44ae53e086ef576ab59568630bb0d6e2ef21ec3a
Muhammad Ali Mustafa <ali.mustafa@qbatch.com>
2025-07-29 12:54:52 +0500

    Update OrderModal Component

--- frontend/src/components/OrderModal.jsx
+++ frontend/src/components/OrderModal.jsx
@@ -83,7 +83,7 @@ function OrderModal({ isOpen, onClose, onSubmit, restaurantName }) {
                                         )}
                                     </div>
                                 ))}
-                                <button onClick={handleAddFields} className='bg-grey-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'>Add Input Field</button>
+                                <button onClick={handleAddFields} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'>Add Input Field</button>
                             </div>
                             <div className="col-span-2">
                                 <label htmlFor="instructions" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" >
