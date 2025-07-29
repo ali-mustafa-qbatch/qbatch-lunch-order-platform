ea9e49fa31c9d2a3d973918d77dc9d9df43e08a8
Muhammad Ali Mustafa <ali.mustafa@qbatch.com>
2025-07-29 10:46:11 +0500

    Remove top padding from PastOrders Component

--- frontend/src/components/PastOrders.jsx
+++ frontend/src/components/PastOrders.jsx
@@ -94,7 +94,7 @@ function PastOrders() {
     }
 
     return (
-    <section className="py-12 px-6 max-w-4xl mx-auto" id="past-orders">
+    <section className="pb-12 px-6 max-w-4xl mx-auto" id="past-orders">
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
