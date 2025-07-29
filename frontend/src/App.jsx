c627944d669b2dca7af4ba93cef355921c092dce
Muhammad Ali Mustafa <ali.mustafa@qbatch.com>
2025-07-29 17:18:27 +0500

    Add Sign in and Sign Up components and configured their routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage"
 import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<HomePage />}> </Route>
 				<Route path="/sign-in" element={<SignIn />}> </Route>
+				<Route path="/sign-up" element={<SignUp />}> </Route>
 			</Routes>
 		</BrowserRouter>
 	)
