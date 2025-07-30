c451ba70b07a0ce0dae82d40b163d0e000cbfcbc
Muhammad Ali Mustafa <ali.mustafa@qbatch.com>
2025-07-30 11:11:54 +0500

    Implement password toggle on sign up and sign in components

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -1,7 +1,14 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
+import { useState } from 'react';
 
 function SignIn() {
+    const [passwordToggle, setPasswordToggle] = useState(false);
+
+    const handlePasswordToggle = () => {
+        setPasswordToggle(!passwordToggle);
+    }
+
     return (
         <>
             <Navbar />
@@ -22,8 +29,8 @@ function SignIn() {
                                 <div>
                                     <label className="text-slate-900 text-sm font-medium mb-2 block">Password</label>
                                     <div className="relative flex items-center">
-                                        <input name="password" type="password" required className="w-full text-slate-900 text-sm border border-slate-300 px-4 py-3 pr-8 rounded-md outline-blue-600" placeholder="Enter password" />
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-4 h-4 absolute right-4 cursor-pointer" viewBox="0 0 128 128">
+                                        <input name="password" type={passwordToggle ? "text" : "password"} required className="w-full text-slate-900 text-sm border border-slate-300 px-4 py-3 pr-8 rounded-md outline-blue-600" placeholder="Enter password" />
+                                        <svg onClick={handlePasswordToggle} xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-4 h-4 absolute right-4 cursor-pointer" viewBox="0 0 128 128">
                                             <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
                                         </svg>
                                     </div>
