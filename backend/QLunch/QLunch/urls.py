53c78b29702e35f39488296adeb8ec281f90c3da
Muhammad Ali Mustafa <ali.mustafa@qbatch.com>
2025-08-05 10:51:48 +0500

    add orders model and define its views and urls

--- backend/QLunch/QLunch/urls.py
+++ backend/QLunch/QLunch/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("orders/", include("orders.urls")),
+    path("api/orders/", include("orders.urls")),
+    path("api/restaurants/", include("restaurants.urls")),
     path('admin/', admin.site.urls),
 ]
--- backend/QLunch/orders/models.py
+++ backend/QLunch/orders/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from restaurants.models import Restaurant
 
 # Create your models here.
+class Orders(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
