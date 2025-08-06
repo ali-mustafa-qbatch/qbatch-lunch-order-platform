c5ac4da359347070856ea866b6ce66ab9cbe7c54
Muhammad Ali Mustafa <ali.mustafa@qbatch.com>
2025-08-06 12:32:50 +0500

    update users views and urls to handle jwt handling

--- backend/QLunch/users/urls.py
+++ backend/QLunch/users/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
     path("register", views.register_user, name="register"),
     path("login", views.login_user, name="login"),
+    path("token/refresh", views.refresh_token, name="refresh_token"),
     path("logout", views.logout_user, name="logout"),
-    # path("profile", views.get_user_profile, name="profile"),
+    path("profile", views.get_user_profile, name="profile"),
     # path("profile/update", views.update_user_profile, name="update_profile"),
     # path("profile/change-password", views.change_user_password, name="change_password"),
     # path("profile/delete", views.delete_user_account, name="delete_account"),
--- backend/QLunch/users/views.py
+++ backend/QLunch/users/views.py
@@ -1,9 +1,10 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from django.contrib.auth import authenticate
 from .serializers import RegisterSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['POST'])
 def register_user(request):
@@ -33,11 +34,35 @@ def login_user(request):
     return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
 @api_view(['POST'])
+def refresh_token(request):
+    refresh_token = request.data.get('refresh_token')
+    if not refresh_token:
+        return Response({"detail": "Refresh token is required."}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        try:
+            refresh = RefreshToken(refresh_token)
+            access_token = str(refresh.access_token)
+            return Response({'access_token': access_token}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def logout_user(request):
-    try:
-        refresh_token = request.data.get('refresh_token')
-        token = RefreshToken(refresh_token)
-        token.blacklist()
-        return Response({"message": "User logged out successfully."}, status=status.HTTP_205_RESET_CONTENT)
-    except Exception as e:
-        return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    if request.user.is_authenticated:
+        try:
+            refresh_token = request.data.get('refresh_token')
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "User logged out successfully."}, status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response({"status": "Not Authenticated"}, status=401)
+    
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user_profile(request):
+    if request.user.is_authenticated:
+        return Response({"status": "Authenticated"})
+    return Response({"status": "Not Authenticated"}, status=401)
