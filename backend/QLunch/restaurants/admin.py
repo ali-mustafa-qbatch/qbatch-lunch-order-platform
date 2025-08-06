f663cb5f55b9ab597bb0141b57e78190449a8016
Muhammad Ali Mustafa <ali.mustafa@qbatch.com>
2025-08-06 17:27:16 +0500

    add restaurants management to admin interface

--- backend/QLunch/restaurants/admin.py
+++ backend/QLunch/restaurants/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Restaurant, MenuImage
 
-# Register your models here.
+class MenuImageInline(admin.TabularInline): 
+    model = MenuImage
+    extra = 1 
+    fields = ['image']  
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_created', 'date_updated')
+    search_fields = ('name',)
+    list_filter = ('date_created',)
+    inlines = [MenuImageInline]  
+
+admin.site.register(Restaurant, RestaurantAdmin)
