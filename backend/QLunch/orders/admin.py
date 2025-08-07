cd338d7cbe99dc361a1ac92c0c084e1879235ce0
Muhammad Ali Mustafa <ali.mustafa@qbatch.com>
2025-08-07 12:14:34 +0500

    add orders in admin interface and add dropdown list for status attribute in order model

--- backend/QLunch/orders/admin.py
+++ backend/QLunch/orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Order
 
 # Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('restaurant', 'customer', 'total_price', 'status', 'date_created', 'date_updated')
+    search_fields = ('restaurant__name', 'customer__username')
+    list_filter = ('status', 'date_created')
+    readonly_fields = ('date_created', 'date_updated')
+
+admin.site.register(Order, OrderAdmin)
--- backend/QLunch/orders/models.py
+++ backend/QLunch/orders/models.py
@@ -6,11 +6,16 @@ User = get_user_model()
 
 # Create your models here.
 class Order(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('delivered', 'Delivered'),
+        ('cleared', 'Cleared'),
+    ]
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='orders')
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
     items = models.JSONField() 
     total_price = models.IntegerField(default=-1)
-    status = models.CharField(max_length=50, default='pending') 
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='pending') 
     instructions = models.TextField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
