835e724b577c0065174557eea2d7be4460d55d91
Muhammad Ali Mustafa <ali.mustafa@qbatch.com>
2025-08-07 12:36:31 +0500

    update read only fields for orders in admin interface

--- backend/QLunch/orders/admin.py
+++ backend/QLunch/orders/admin.py
@@ -6,6 +6,6 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('restaurant', 'customer', 'total_price', 'status', 'date_created', 'date_updated')
     search_fields = ('restaurant__name', 'customer__username')
     list_filter = ('status', 'date_created')
-    readonly_fields = ('date_created', 'date_updated')
+    readonly_fields = ('date_created', 'date_updated', 'customer', 'items', 'instructions', 'restaurant')
 
 admin.site.register(Order, OrderAdmin)
