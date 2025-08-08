3fe8f721e1f86251b718ceee5eab246adfbdb4c6
Muhammad Ali Mustafa <ali.mustafa@qbatch.com>
2025-08-08 10:48:41 +0500

    update settings file to correctly handle env

--- backend/QLunch/QLunch/settings.py
+++ backend/QLunch/QLunch/settings.py
@@ -18,8 +18,9 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-# reading .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+env_file = os.path.join(BASE_DIR, '.env')
+if os.path.exists(env_file):
+    environ.Env.read_env(env_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
