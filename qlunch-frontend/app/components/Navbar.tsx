d3ada4b6d4d4ba15e57c97fe05a079a1f736fff9
Muhammad Ali Mustafa <ali.mustafa@qbatch.com>
2025-08-13 11:09:15 +0500

    update hero section

--- qlunch-frontend/app/components/Navbar.tsx
+++ qlunch-frontend/app/components/Navbar.tsx
@@ -25,7 +25,7 @@ export const Navbar = () => {
     };
 
     return (
-        <nav className="bg-white">
+        <nav className="bg-transparent fixed top-0 left-0 w-full z-50">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center h-16">
                     <div className="flex-shrink-0">
--- qlunch-frontend/app/routes/home.tsx
+++ qlunch-frontend/app/routes/home.tsx
@@ -2,6 +2,7 @@ import type { Route } from "./+types/home";
 import { AuthProvider } from "../context/AuthContext";
 import { Navbar } from "~/components/Navbar";
 import { HomePage } from "~/components/HomePage";
+import ArcGalleryHero from "~/components/arc-gallery-hero";
 import { Footer } from "~/components/Footer";
 
 export function meta({}: Route.MetaArgs) {
@@ -12,10 +13,25 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const images = [
+  '/mudassir-zaheer-V6XrszoqjNk-unsplash.jpg', 
+  '/anh-nguyen-kcA-c3f_3FE-unsplash.jpg', 
+  '/anna-tukhfatullina-food-photographer-stylist-Mzy-OjtCI70-unsplash.jpg',
+  '/alex-munsell-Yr4n8O_3UPc-unsplash.jpg',
+  '/kalpa-mahagamage-LxkWpGMEwlM-unsplash.jpg',
+  '/umair-ali-asad-rRFE8agWJNw-unsplash.jpg',
+  '/ananthan-chithiraikani-FAjiQN9xowE-unsplash.jpg',
+  '/shourav-sheikh-a66sGfOnnqQ-unsplash.jpg',
+  '/joshua-kantarges-wO4CyWnIzz4-unsplash.jpg',
+  'https://images.unsplash.com/photo-1627308595229-7830a5c91f9f',
+  '/joseph-gonzalez-zcUgjyqEwe8-unsplash.jpg'
+];
+
   return (
     <AuthProvider>
       <main>
         <Navbar />
+        <ArcGalleryHero images={images} />
         <HomePage />
         <Footer />
       </main>
